75c90c67176cfc1089f4c21926c3926f8a759d79
Daniel Schworm <danielschworm@t-online.de>
2019-10-14 09:03:59 +0200

    Add app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css', './sidebar/sidebar.component.css', './header/header.component.css'],
+    styleUrls: ['./app.component.css','./header/header.component.css'],
 })
 
 export class AppComponent {

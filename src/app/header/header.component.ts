f7129b911453fdd3018cf7a1b05112b70150d4f9
Daniel Schworm <danielschworm@t-online.de>
2019-10-22 09:48:08 +0200

    Add new list checkbox

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,20 +22,4 @@ export class HeaderComponent implements OnInit {
     this.appComponent.gridSidebarToggle(this.collapsed);
     this.collapsed=!this.collapsed;
   }
-
-
-  showTime(): void {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-
-    var time = h + ":" + m + ":" + s;
-    document.getElementById("clock").innerText = time;
-    document.getElementById("clock").textContent = time;
-
-    }
-
-    x = setInterval (() => {this.showTime ();}, 1000);
-
 }
